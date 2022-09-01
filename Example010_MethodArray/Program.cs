1b2313f73e8de246204bd62161c017bfba2b11c8
Alexandr <crazy.top@mail.ru>
2022-09-01 11:34:11 +0300

    Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 31, 4, 18, 152, 16, 24, 46};
+
+int n = array.Length;
+int find = 152;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
